4c17e4cd59f5eb087794f3869c6bf4964127c363
Sulaim-Saadath <avoor2005@gmail.com>
2026-08-14 07:16:00 +0530

    BubbleSort

---
+++ Arrays/src/com/practice/Sorting/BubbleSort.java
@@ -0,0 +1,43 @@
+package com.practice.Sorting;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+// 12 45 7 19 2
+// itr 1 -> 
+//     12 < 45
+//     45 < 7(swap)  
+//     45 < 19(swap)
+//     45 < 2(swap)
+// 12 7 19 2 45
+// itr 2 -> 
+//     12 < 7 (swap)
+//     12 < 19
+//     19 < 2 (swap)
+// 7 12 2 19 45
+public class BubbleSort {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the size of array: ");
+		int[] arr = new int[scan.nextInt()];
+		System.out.println("Enter the elements inside the array: ");
+		for (int i = 0; i <= arr.length - 1; i++) {
+			arr[i] = scan.nextInt();
+		}
+		scan.close();
+		bubbleSort(arr);
+		System.out.println("Sorted Array is: " + Arrays.toString(arr));
+	}
+	
+	public static void bubbleSort(int[] arr) {
+		for(int i = 0;i <= arr.length - 1;i++) {
+			for(int j = 0;j <= arr.length - i - 2;j++) {
+				if(arr[j] > arr[j+1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
+	}
+}
