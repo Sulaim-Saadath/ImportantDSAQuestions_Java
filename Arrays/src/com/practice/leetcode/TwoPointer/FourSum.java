9d7d08c67815c6d0d03f412a69f0d6d61521e66e
Sulaim-Saadath <avoor2005@gmail.com>
2026-08-27 10:37:49 +0530

    .

--- Arrays/src/com/practice/leetcode/TwoPointer/FourSum.java
+++ Arrays/src/com/practice/leetcode/TwoPointer/FourSum.java
@@ -19,6 +19,7 @@ public class FourSum {
 		int target = scan.nextInt();
 		scan.close();
 		Arrays.sort(arr);
+		System.out.println("The sorted array: " + Arrays.toString(arr));
 		List<List<Integer>> resultList = fourSum(arr, target);
 		System.out.println(resultList);
 	}
@@ -26,11 +27,11 @@ public class FourSum {
 	public static List<List<Integer>> fourSum(int[] arr, int target) {
 		List<List<Integer>> list = new ArrayList<List<Integer>>();
 		for (int i = 0; i <= arr.length - 4; i++) {
-			if(i > 0 && arr[i - 1] == arr[i]) {
+			if (i > 0 && arr[i - 1] == arr[i]) {
 				continue;
 			}
 			for (int j = i + 1; j <= arr.length - 3; j++) {
-				if(j > i + 1 && arr[j - 1] == arr[j]) {
+				if (j > i + 1 && arr[j - 1] == arr[j]) {
 					continue;
 				}
 				int left = j + 1;
@@ -46,9 +47,9 @@ public class FourSum {
 						list.add(addList);
 						left++;
 						right--;
-						while(left < right && arr[left - 1] == arr[left]) 
+						while (left < right && arr[left - 1] == arr[left])
 							left++;
-						while(right > left && arr[right + 1] == arr[right]) 
+						while (right > left && arr[right + 1] == arr[right])
 							right--;
 					} else if (sum < target) {
 						left++;
